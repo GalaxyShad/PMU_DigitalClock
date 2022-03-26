802c2b51d23dfae52e2a86666ff0316fc588ece6
Емельянов Сергей <esn@medinfocenter.ru>
2022-03-26 09:24:41 +0500

    работа над ошибками

--- PMU_DigitalClock/PMU_DigitalClock/MainPage.xaml.cs
+++ PMU_DigitalClock/PMU_DigitalClock/MainPage.xaml.cs
@@ -1,62 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace PMU_DigitalClock
 {
     public partial class MainPage : ContentPage
     {
-        static DigitBox[] digitBoxes = new DigitBox[6];
+        private readonly DigitBox[] digitBoxes = new DigitBox[6];
+
         public MainPage()
         {
             InitializeComponent();
 
-            for (int i = 0; i < 6; i++)
+            AbsoluteLayout CreateDots(int x, int y)
             {
-                digitBoxes[i] = new DigitBox(clockLayout, i * 64 - (8 * (i % 2)), 0);
-                digitBoxes[i].Set(0);
+                var layout = new AbsoluteLayout();
+                for (int i = 0; i < 2; i++)
+                {
+                    BoxView boxView = new BoxView();
+                    boxView.BackgroundColor = Color.FromHex("#9FC131");
+                    boxView.CornerRadius = 32;
+                    layout.Children.Add(boxView, new Rectangle(x, y + i * 32, 8, 8));
+                }
+                return layout;
             }
 
-            CreateDots(112, 24);
-            CreateDots(240, 24);
+            for (var i = 0; i < 6; i++)
+            {
+                var digitBox = new DigitBox();
+                digitBoxes[i] = digitBox;
+                clockLayout.Children.Add(digitBox);
+
+                if (i % 2 == 1 && i < 5)
+                {
+                    var dots = CreateDots(0, 24);
+                    dots.Margin = new Thickness(8, 0);
+                    clockLayout.Children.Add(dots);
+                }
+            }
 
             Device.StartTimer(TimeSpan.FromSeconds(1), OnTimer);
             OnTimer();
-
         }
 
         bool OnTimer()
         {
-            DateTime dateTime = DateTime.Now;
+            var dateTime = DateTime.Now.ToString("HHmmss");
 
-            digitBoxes[0].Set(dateTime.Hour / 10);
-            digitBoxes[1].Set(dateTime.Hour % 10);
-            digitBoxes[2].Set(dateTime.Minute / 10);
-            digitBoxes[3].Set(dateTime.Minute % 10);
-            digitBoxes[4].Set(dateTime.Second / 10);
-            digitBoxes[5].Set(dateTime.Second % 10);
+            for (var i = 0; i < dateTime.Length; i++)
+            {
+                var digitBox = digitBoxes[i];
+                var value = Convert.ToInt32(dateTime[i].ToString());
+                digitBox.Set(value);
+            }
+
+            //digitBoxes[0].Set(dateTime.Hour / 10);
+            //digitBoxes[1].Set(dateTime.Hour % 10);
+            //digitBoxes[2].Set(dateTime.Minute / 10);
+            //digitBoxes[3].Set(dateTime.Minute % 10);
+            //digitBoxes[4].Set(dateTime.Second / 10);
+            //digitBoxes[5].Set(dateTime.Second % 10);
 
             return true;
-        }
-
-        void CreateDots(int x, int y)
-        {
-            for (int i = 0; i < 2; i++)
-            {
-                BoxView boxView = new BoxView();
-                boxView.BackgroundColor = Color.FromHex("#9FC131");
-                boxView.CornerRadius = 32;
-                clockLayout.Children.Add(boxView, new Rectangle(x, y + i * 32, 8, 8));
-            }
         }
     }
 
 
-    public class DigitBox
+    public class DigitBox : AbsoluteLayout
     {
         readonly Color colOff = Color.FromHex("#005C53");
         readonly Color colOn = Color.FromHex("#DBF227");
@@ -80,35 +89,37 @@ namespace PMU_DigitalClock
             {true, true, true, true, false, true, true},        // 9
         };
 
-        public DigitBox(AbsoluteLayout layout, int x, int y)
+        public DigitBox()
         {
             for (int i = 0; i < 7; i++)
             {
-                segments[i] = new BoxView();
-                segments[i].BackgroundColor = colOff;
-                segments[i].CornerRadius = 32;
+                segments[i] = new BoxView
+                {
+                    BackgroundColor = colOff,
+                    CornerRadius = 32
+                };
             }
 
-            layout.Children.Add(segments[0],
-                    new Rectangle(x + segThick, y, segLen, segThick));
-            layout.Children.Add(segments[1],
-                new Rectangle(x + segThick + segLen, y + segThick, segThick, segLen));
-            layout.Children.Add(segments[2],
-                new Rectangle(x + segThick + segLen, y + segThick * 2 + segLen, segThick, segLen));
-            layout.Children.Add(segments[3],
-                new Rectangle(x + segThick, y + segThick * 2 + segLen * 2, segLen, segThick));
-            layout.Children.Add(segments[4],
-                new Rectangle(x, y + segThick * 2 + segLen, segThick, segLen));
-            layout.Children.Add(segments[5],
-                new Rectangle(x, y + segThick, segThick, segLen));
-            layout.Children.Add(segments[6],
-                new Rectangle(x + segThick, y + segThick + segLen, segLen, segThick));
+            Children.Add(segments[0],
+                    new Rectangle(segThick, 0, segLen, segThick));
+            Children.Add(segments[1],
+                new Rectangle(segThick + segLen, segThick, segThick, segLen));
+            Children.Add(segments[2],
+                new Rectangle(segThick + segLen, segThick * 2 + segLen, segThick, segLen));
+            Children.Add(segments[3],
+                new Rectangle(segThick, segThick * 2 + segLen * 2, segLen, segThick));
+            Children.Add(segments[4],
+                new Rectangle(0, segThick * 2 + segLen, segThick, segLen));
+            Children.Add(segments[5],
+                new Rectangle(0, segThick, segThick, segLen));
+            Children.Add(segments[6],
+                new Rectangle(segThick, segThick + segLen, segLen, segThick));
         }
 
         public void Set(int digit)
         {
             if (digit < 0 || digit > 9)
-                return;
+                throw new ArgumentOutOfRangeException(nameof(digit));
 
             for (int i = 0; i < 7; i++)
             {
